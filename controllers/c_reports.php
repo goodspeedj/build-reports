ac99f599fdf559d6a4245a99a92c9e928538dc94
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-12-04 14:21:20 -0500

    Base controller files for reports and records

---
+++ controllers/c_reports.php
@@ -0,0 +1,11 @@
+<?php
+class reports_controller extends base_controller {
+
+    public function __construct() {
+        parent::__construct();
+
+        if(!$this->user) {
+            Router::redirect('/users/login');
+        }
+    } 
+}
