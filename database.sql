5275c8bdc6233a7d340878b966f6c96abbfef9d4
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-12-05 16:42:12 -0500

    New database def file

--- database.sql
+++ database.sql
@@ -1,25 +1,27 @@
--- phpMyAdmin SQL Dump
--- version 3.5.7
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.1.61, for redhat-linux-gnu (x86_64)
 --
--- Host: localhost
--- Generation Time: Dec 05, 2013 at 03:36 AM
--- Server version: 5.5.29
--- PHP Version: 5.4.10
+-- Host: localhost    Database: odspeedc_p4_go_odspeed_com
+-- ------------------------------------------------------
+-- Server version	5.1.61
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
---
--- Database: `odspeedc_p4_go_odpeed_com`
---
-
--- --------------------------------------------------------
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `builds`
 --
 
+DROP TABLE IF EXISTS `builds`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `builds` (
   `build_id` int(11) NOT NULL AUTO_INCREMENT,
   `component_id` int(11) NOT NULL,
@@ -31,21 +33,26 @@ CREATE TABLE `builds` (
   PRIMARY KEY (`build_id`),
   UNIQUE KEY `version_id_UNIQUE` (`build_id`),
   KEY `component_id_idx` (`component_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `builds`
 --
 
-INSERT INTO `builds` (`build_id`, `component_id`, `build_num`, `created`, `status`, `duration`, `job_name`) VALUES
-(1, 1, '1', 0, 'Unstable', 45.4, 'PMESalesClient_January');
-
--- --------------------------------------------------------
+LOCK TABLES `builds` WRITE;
+/*!40000 ALTER TABLE `builds` DISABLE KEYS */;
+INSERT INTO `builds` VALUES (1,1,'1',1386279476,'Unstable',45.4,'PMESalesClient_January'),(2,1,'2',1386279555,'Stable',50,'PMESalesClient_December'),(3,7,'12',1386279383,'Stable',32.4,'PMESalesDesktopAutoClient_January'),(4,10,'12',1386279431,'Failed',56.3,'PmEServiceBilling January');
+/*!40000 ALTER TABLE `builds` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `components`
 --
 
+DROP TABLE IF EXISTS `components`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `components` (
   `component_id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` int(11) NOT NULL,
@@ -53,49 +60,54 @@ CREATE TABLE `components` (
   `desc` varchar(255) NOT NULL,
   PRIMARY KEY (`component_id`),
   UNIQUE KEY `component_id_UNIQUE` (`component_id`),
-  KEY `product_id_idx` (`product_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+  KEY `product_id_idx` (`product_id`),
+  CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `components`
 --
 
-INSERT INTO `components` (`component_id`, `product_id`, `name`, `desc`) VALUES
-(1, 1, 'Client', 'eSales Client'),
-(2, 1, 'Mobile', 'eSales Mobile'),
-(3, 1, 'Analytics', 'eSales Analytics'),
-(4, 2, 'Services', 'eService Services'),
-(5, 3, 'Services', 'NextGen Services');
-
--- --------------------------------------------------------
+LOCK TABLES `components` WRITE;
+/*!40000 ALTER TABLE `components` DISABLE KEYS */;
+INSERT INTO `components` VALUES (1,1,'Client','eSales Client'),(2,1,'Mobile','eSales Mobile'),(3,1,'Analytics','eSales Analytics'),(4,2,'Services','eService Services'),(5,3,'Services','NextGen Services'),(6,1,'Common Client',''),(7,1,'Desktop Auto Client',''),(8,1,'Desktop Common Client',''),(9,1,'Desktop Property Client',''),(10,2,'Billing',''),(11,2,'Claims',''),(12,2,'Policy',''),(13,2,'Web Account Apps',''),(14,2,'Web Application Libraries',''),(15,2,'Web Billing Apps',''),(16,2,'Web Libraries',''),(17,2,'Web Policy Apps',''),(18,4,'Common Client','');
+/*!40000 ALTER TABLE `components` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `products`
 --
 
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `products` (
   `product_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `desc` varchar(255) NOT NULL,
   PRIMARY KEY (`product_id`),
   UNIQUE KEY `product_id_UNIQUE` (`product_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`product_id`, `name`, `desc`) VALUES
-(1, 'eSales', 'NextGen eSales'),
-(2, 'eService', 'eService'),
-(3, 'Services', 'NextGen Services');
-
--- --------------------------------------------------------
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'eSales','NextGen eSales'),(2,'eService','eService'),(3,'Services','NextGen Services'),(4,'Internet','');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `created` int(11) NOT NULL,
@@ -109,14 +121,25 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_id_UNIQUE` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Constraints for dumped tables
+-- Dumping data for table `users`
 --
 
---
--- Constraints for table `components`
---
-ALTER TABLE `components`
-  ADD CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-12-05 16:41:05
