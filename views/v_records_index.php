672a0ce271f65ae705e58bc36a4be53a78f1d661
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-12-05 16:03:39 -0500

    Added created column

--- views/v_records_index.php
+++ views/v_records_index.php
@@ -7,6 +7,7 @@
       <th>Job Name</th>
       <th>Status</th>
       <th>Build Time</th>
+      <th>Created</th>
     </tr>
   </thead>
   <tbody>
@@ -20,6 +21,7 @@
       <td><?= $row['job_name']; ?></td>
       <td><?= $row['status']; ?></td>
       <td><?= $row['duration']; ?></td>
+      <td><?= $row['created']; ?></td>
     </tr>
   <?php endforeach; ?>
   
