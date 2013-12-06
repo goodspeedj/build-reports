6f621d7485df3f88a5ee70a4f353e33196069214
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-12-06 15:26:46 -0500

    Basic record add form

--- views/v_records_add.php
+++ views/v_records_add.php
@@ -1,14 +1,29 @@
 <form id="record-add" method="POST" action="/records/p_add">
-  <pre>
-    <?= $products ?>
-  </pre>
   <select name="product">
-    <option>eSales</option>
-    <option>eService</option>
+
+  <?php foreach ($products as $product): ?>
+    <option><?= $product['name']; ?></option>
+  <?php endforeach; ?>
+
   </select>
+
   <select name="component">
-    <option>eSales</option>
-    <option>eService</option>
+
+  <?php foreach ($components as $component): ?>
+    <option><?= $component['name']; ?></option>
+  <?php endforeach; ?>
+
   </select>
-	<input type="text" name="product" placeholder="" required><br />
+
+  <input type="text" name="build_num" placeholder="Build #" required>
+  <input type="text" name="job_name" placeholder="Job Name" required>
+
+  <select name="status">
+    <option>Stable</option>
+    <option>Unstable</option>
+    <option>Failed</option>
+  </select>
+
+  <input type="text" name="duration" placeholder="Build Time" required>
+
 </form>
