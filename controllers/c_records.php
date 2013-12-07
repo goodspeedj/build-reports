d78165a7e81b4ff48f2020b0ba7590e0b38b694e
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-12-06 22:12:16 -0500

    Added check for logged in user

--- controllers/c_records.php
+++ controllers/c_records.php
@@ -4,9 +4,9 @@ class records_controller extends base_controller {
     public function __construct() {
         parent::__construct();
 
-        //if(!$this->user) {
-        //    Router::redirect('/records/index');
-        //}
+        if(!$this->user) {
+            Router::redirect('/records/index');
+        }
     } 
 
 
