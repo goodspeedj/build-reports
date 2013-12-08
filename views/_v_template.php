7d3e36703edbde41ef1347154bba0d59b9ec39c9
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-12-07 21:20:21 -0500

    Fixed some layout issues

--- views/_v_template.php
+++ views/_v_template.php
@@ -32,12 +32,10 @@
     <!-- Display main content -->
     <div class="container">
       <div class="row">
-        <div class="span3"></div>
-        <div class="span6 pagination-centered">
+        <div class="col-md-12 pagination-centered">
           <?php if(isset($content)) echo $content; ?>
           <?php if(isset($client_files_body)) echo $client_files_body; ?>
         </div>
-        <div class="span3"></div>
       </div>
     </div>
 
--- views/v_users_login.php
+++ views/v_users_login.php
@@ -1,30 +1,38 @@
-<!-- Display the user login form -->
-<form id="form-signin" method="post" action="/users/p_login">
+<div class="container">
+  <div class="col-md-4"></div>
+  <div class="col-md-4">
 
-  <h3 class="form-signin-heading">Please sign in</h3>
-  <input type="email" name="email" placeholder="Email address" required>
-  <br />
-  <input type="password" name="password" placeholder="Password" required>
-    
-  <!-- Check for bad user name / password -->
-  <?php if(isset($err)): ?>
+    <!-- Display the user login form -->
+    <form id="form-signin" method="post" action="/users/p_login">
+      <div class="form-group">
 
-    <div class='profile alert alert-danger'>
-      Login failed, please check credentials.
-    </div>
-    <br>
+        <h3 class="form-signin-heading">Please sign in</h3>
+        <input class="form-control" type="email" name="email" placeholder="Email address" required>
+        <br /><br />
+        <input class="form-control" type="password" name="password" placeholder="Password" required>
+          
+        <!-- Check for bad user name / password -->
+        <?php if(isset($err)): ?>
 
-  <?php endif; ?>
+          <div class='profile alert alert-danger'>
+            Login failed, please check credentials.
+          </div>
+          <br>
+
+        <?php endif; ?>
 
 
 
-  <p><a href="/users/signup">Register for account</a></p>
-  <p>&nbsp;</p>
-    
-  <button class="btn" type="submit">Log in</button>
-  
-</form>
+        <p><a href="/users/signup">Register for account</a></p>
+        <p>&nbsp;</p>
+          
+        <button class="btn" type="submit">Log in</button>
+      </div>
+    </form>
 
-<script>
-  $("#form-signin").validate();
-</script>
+    <script>
+      $("#form-signin").validate();
+    </script>
+  </div>
+  <div class="col-md-4"></div>
+</div>
