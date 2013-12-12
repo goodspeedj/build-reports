5522c84fbf6ee3ac7220b83d3ccf30187de8b1b8
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-12-12 16:05:29 -0500

    latest changes

--- views/v_reports_scatter.php
+++ views/v_reports_scatter.php
@@ -6,29 +6,33 @@
   <script>
 
     var data = <?php echo json_encode($data); ?>;
-    
+    //var data = [
+    //            [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
+    //            [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
+    //          ];    
+
     var format = d3.time.format("%m/%d/%y");
 
     var margin = {top: 20, right: 30, bottom: 30, left: 40},
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
-    var svg = d3.select("#graph")
-    			.append("svg")
-    			.attr("width", width)
-    			.attr("height", height);
+    var svg = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
 
-   	d3.selectAll("circle")
-   		.data(data)
-   		.enter()
-   		.append("circle")
-   		.attr("cx", function(d) {
-        	return d[0];
-   		})
-   		.attr("cy", function(d) {
-        	return d[1];
-   		})
-   		.attr("r", 5);
+      svg.selectAll("circle")
+         .data(data)
+         .enter()
+         .append("circle")
+         .attr("cx", function(d) {
+            return d.date;
+         })
+         .attr("cy", function(d) {
+            return d.duration;
+         })
+         .attr("r", 5);
 
   </script>
 </div>
