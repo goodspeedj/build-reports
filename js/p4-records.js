a6d5f73390ba4eec73bd9ee28523d9c73905c517
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-12-18 21:08:11 -0500

    Added in basic form validation

--- js/p4-records.js
+++ js/p4-records.js
@@ -4,3 +4,24 @@ $(document).ready(function() {
         "sDom": "<'row'<'col-xs-6'T><'col-xs-6'f>r>t<'row'<'col-xs-6'i><'col-xs-6'p>>",
     } );
 } );
+
+// Form validation for Add Build form
+$( "#record-add" ).validate({
+  rules: {
+    build_num: {
+      required: true,
+      number: true
+    },
+    job_name: {
+      required: true
+    },
+    duration: {
+      required: true,
+      number: true
+    },
+    coverage: {
+      required: true,
+      number: true
+    }
+  }
+});
