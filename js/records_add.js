1fd52171506acf94f8398597e777e32aca90a6df
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-12-20 13:28:28 -0500

    Added better validation - range of numbers

--- js/records_add.js
+++ js/records_add.js
@@ -3,18 +3,21 @@ $( "#record-add" ).validate({
   rules: {
     build_num: {
       required: true,
-      number: true
+      number: true,
+      range: [1, 500]
     },
     job_name: {
       required: true
     },
     duration: {
       required: true,
-      number: true
+      number: true,
+      range: [1, 1000]
     },
     coverage: {
       required: true,
-      number: true
+      number: true,
+      range: [1, 100]
     }
   }
 });
