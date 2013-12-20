36110c44744624ab5f58d55df055af2e56cd68ca
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-12-19 20:56:22 -0500

    Fixed formatting of the form

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -1,31 +1,43 @@
-<!-- New user sign up form -->
-<h3>Sign up</h3>
+<div class="container">
+  <div class="col-md-4"></div>
+  <div class="col-md-4">
+    <!-- New user sign up form -->
+    <h3>Sign up</h3>
 
-<form id="form-signup" method="POST" action="/users/p_signup">
+    <form id="form-signup" method="POST" action="/users/p_signup">
+      <div class="form-group">
 
-  <input type="text" name="first_name" placeholder="First Name" required><br />
-  <input type="text" name="last_name" placeholder="Last Name" required><br />
-  <input type="email" name="email" placeholder="Email address" required><br />
-  <input type="password" name="password" placeholder="Password" required><br />
+        <input type="text" class="form-control" name="first_name" placeholder="First Name" required><br />
+        <input type="text" class="form-control" name="last_name" placeholder="Last Name" required><br />
+        <input type="email" class="form-control" name="email" placeholder="Email address" required><br />
+        <input type="password" class="form-control" name="password" placeholder="Password" required><br />
 
+        <!-- Error checking -->
+        <?php if(isset($errors) && isset($source)): ?>
 
-  <!-- Error checking -->
-  <?php if(isset($errors) && isset($source)): ?>
+          <?php if ($source == 'email'): ?>
+            <p>&nbsp;</p>
+            <div class='profile alert alert-danger'>
+              Email address is already registered.
+            </div>
+            <br>
+          <?php endif; ?>
 
-    <?php if ($source == 'email'): ?>
-      <p>&nbsp;</p>
-      <div class='profile alert alert-danger'>
-        Email address is already registered.
+        <?php endif; ?>
+
+        <p>&nbsp;</p>
+        <div class="col-sm-4"></div>
+            <div class="col-sm-4 center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+            <div class="col-sm-4"></div>
+
       </div>
-      <br>
-    <?php endif; ?>
-
-  <?php endif; ?>
-
-  <p>&nbsp;</p>
-  <button class="btn" type="submit">Sign up</button>
- 
-</form>
+     
+    </form>
+  </div>
+  <div class="col-md-4"></div>
+</div>
 
 <script>
   $("#form-signup").validate();
