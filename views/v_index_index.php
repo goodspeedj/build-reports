669570c72f3e0d795fb2858d494f19bb81fd2e5e
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-12-20 08:32:09 -0500

    Introduction

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,12 +1,46 @@
 <h3>Welome to the Build Tracker</h3>
 <div class="left">
+
+  <h5>Overview</h5>
+    The Build Tracker is an application used to track Continuous Integration software builds.  
+    I created this application as a proof of concept for a tool that I hope to use at work.  
+    In its current form builds can be manually added to the MySQL database through the UI, but 
+    in future form our build engine (Jenkins) will automatically write the build information 
+    to the database.  In this form the application will be a read only application.  For the 
+    purposes of this project the data is randomized dummy data generated with the createSQL.pl 
+    perl script.
+
+  <h5>Admin</h5>
+    This section is basic administration - editing your user profile and adding new build records.
+
+  <h5>Records</h5>
+    This section is a table displaying all of the build records in tabular form.  A user can page 
+    through the records, sort the records or search/filter the records.  The Data Tables 
+    (http://datatables.net/) javascript library was used for the table layout.
+
+  <h5>Reports</h5>
+    This section has two different reports.  The first report is a stacked area chart showing the 
+    number builds by date and by status (stable, unstable or failed).
+  <p>
+    Both of these reports were generated with the D3 library (http://d3js.org/).
+  </p>
+  <p>
+    The main goal of these reports is to show trending information over time.  Specifically I would 
+    like to know whether the builds taking longer over time, do we have less failures, is our code 
+    coverage improving?  Future enhancements would include the ability to filter the reports on a 
+    specific sub-set of data.
+  </p>
+
   <p>&nbsp;</p>
-  <p>Javascript Libraries used for this project include:</p>
+  <h5>Javascript Libraries used for this project:</h5>
   <ul>
-    <li>Bootstrap</li>
-    <li>Bootstrap File Style</li>
-    <li>jQuery</li>
-    <li>jQuery Validate</li>
+    <li>Bootstrap: Layout Design</li>
+    <li>jQuery: Client side JavaScript Library</li>
+    <li>jQuery Validate: Form Validation</li>
+    <li>D3: Data Driven Documents</li>
+    <li>Data Tables: Table Layout</li>
+    <li>Data Tables Bootstrap: Table Layout with Bootstrap style</li>
+    <li>Moment: Date Time Library</li>
   </ul>
   <p>&nbsp;</p>
 
