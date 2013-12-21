537389815a97a9343d03fabc751645f25fd6795e
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-12-20 20:37:34 -0500

    Fixed logo link

--- views/_v_navbar.php
+++ views/_v_navbar.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#"><?= APP_NAME ?></a>
+      <a class="navbar-brand" href="/index/index"><?= APP_NAME ?></a>
     </div>
     <div class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
