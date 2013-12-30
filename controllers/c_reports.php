d7bb54872e521dc6f883ce4884b3a65fcd742d1b
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-12-30 16:16:22 -0500

    Separate data counts for each product type - needs to be refactored later to one query

--- controllers/c_reports.php
+++ controllers/c_reports.php
@@ -99,9 +99,125 @@ class reports_controller extends base_controller {
                   ELSE 100 END";
 
 
-        $data = DB::instance(DB_NAME)->select_rows($sql);
+        $allData = DB::instance(DB_NAME)->select_rows($sql);
 
-        $this->template->content->data = $data;
+        $sql = "SELECT 
+                  allRecords.date,
+                  allRecords.Name,
+                  ( SELECT 
+                      COUNT(statuses.status_id)
+                    FROM statuses, builds, calendar
+                    WHERE builds.status_id = statuses.status_id
+                      AND FROM_UNIXTIME(builds.created, '%Y-%m-%d') = calendar.date
+                      AND calendar.date = allRecords.date
+                      AND statuses.name = allRecords.Name
+                  ) as Count
+
+                FROM 
+                  ( SELECT calendar.date,
+                      statuses.status_id,
+                      statuses.name
+                    FROM
+                      calendar,
+                      statuses
+                    ORDER BY
+                      calendar.date,
+                      statuses.name  ) allRecords
+
+                LEFT JOIN builds
+                  ON builds.status_id =  allRecords.status_id
+
+                LEFT JOIN statuses
+                  ON builds.status_id = statuses.status_id
+
+                LEFT JOIN components
+                  ON builds.component_id = components.component_id
+
+                LEFT JOIN products
+                  ON products.product_id = components.product_id
+
+                WHERE allRecords.date BETWEEN 
+                  (SELECT MIN(FROM_UNIXTIME(builds.created, '%Y-%m-%d')) FROM builds) 
+                AND 
+                  (SELECT MAX(FROM_UNIXTIME(builds.created, '%Y-%m-%d')) FROM builds)
+                AND 
+                  products.name = 'Sales'
+
+                GROUP BY
+                  allRecords.date,
+                  allRecords.Name
+
+                ORDER BY 
+                  allRecords.date,
+                  CASE allRecords.Name
+                    WHEN 'Stable' THEN 1
+                    WHEN 'Unstable' THEN 2
+                    WHEN 'Failed' THEN 3
+                  ELSE 100 END";
+
+
+        $salesData = DB::instance(DB_NAME)->select_rows($sql);
+
+        $this->template->content->salesData = $salesData;
+
+        $sql = "SELECT 
+                  allRecords.date,
+                  allRecords.Name,
+                  ( SELECT 
+                      COUNT(statuses.status_id)
+                    FROM statuses, builds, calendar
+                    WHERE builds.status_id = statuses.status_id
+                      AND FROM_UNIXTIME(builds.created, '%Y-%m-%d') = calendar.date
+                      AND calendar.date = allRecords.date
+                      AND statuses.name = allRecords.Name
+                  ) as Count
+
+                FROM 
+                  ( SELECT calendar.date,
+                      statuses.status_id,
+                      statuses.name
+                    FROM
+                      calendar,
+                      statuses
+                    ORDER BY
+                      calendar.date,
+                      statuses.name  ) allRecords
+
+                LEFT JOIN builds
+                  ON builds.status_id =  allRecords.status_id
+
+                LEFT JOIN statuses
+                  ON builds.status_id = statuses.status_id
+
+                LEFT JOIN components
+                  ON builds.component_id = components.component_id
+
+                LEFT JOIN products
+                  ON products.product_id = components.product_id
+
+                WHERE allRecords.date BETWEEN 
+                  (SELECT MIN(FROM_UNIXTIME(builds.created, '%Y-%m-%d')) FROM builds) 
+                AND 
+                  (SELECT MAX(FROM_UNIXTIME(builds.created, '%Y-%m-%d')) FROM builds)
+                AND 
+                  products.name = 'Service'
+
+                GROUP BY
+                  allRecords.date,
+                  allRecords.Name
+
+                ORDER BY 
+                  allRecords.date,
+                  CASE allRecords.Name
+                    WHEN 'Stable' THEN 1
+                    WHEN 'Unstable' THEN 2
+                    WHEN 'Failed' THEN 3
+                  ELSE 100 END";
+
+
+        $serviceData = DB::instance(DB_NAME)->select_rows($sql);
+
+        $this->template->content->serviceData = $serviceData;
 
         // Display the view
         echo $this->template;
